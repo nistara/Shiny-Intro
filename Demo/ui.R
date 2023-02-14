a1d6fdb8c4caf0a9a534683e7961280ee1fc89e4
nistara <nrandhawa@ucdavis.edi>
2023-02-14 19:11:34 +0000

    test changes

--- Demo/ui.R
+++ Demo/ui.R
@@ -13,7 +13,7 @@ library(shiny)
 fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Old Faithful Geyser Data Made Some Changes"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
